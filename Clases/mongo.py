bb6ba889e75973f1fa23c2061a50f65913aa776e
jesusdanielgascaalvrado@gmail.com <jesusdanielgascaalvrado@gmail.com>
2023-04-25 14:03:02 -0600

    fin2000

--- Clases/mongo.py
+++ Clases/mongo.py
@@ -8,7 +8,7 @@ class MongoDBClient(Jsonn):
         self.lista2=[]
         self.uri = uri
         self.client=None
-        self.listatempo=[]
+        
     
     def connect(self):
         urls = [self.uri, "mongodb://54.175.50.139:27018/?directConnection=true"]
@@ -29,32 +29,29 @@ class MongoDBClient(Jsonn):
     
     
     def update_all_documents(self, db_name, coll_name, new_docs):
-     try:
-        db = self.client[db_name]
-        coll = db[coll_name]
-        
-        # Check if temp.json exists
-        if os.path.exists("temp.json"):
-            print("Encontramos temp")
-            with open('temp.json','r') as file:
-                self.listatempo = json.load(file)
+        try:
+            if os.path.exists("temp.json"):
                 
-            # Add documents from temp.json to new_docs
-            new_docs.extend(self.listatempo)
-            
-            # Insert all documents into MongoDB
-            coll.insert_many(new_docs)
-            
-            # Remove temp.json
-            os.remove("temp.json")
-            
-        else:
-            # Insert new_docs into MongoDB
-            coll.insert_many(new_docs)
-            
-     except Exception as e:
-        print("No se puede conectar a ningún servidor")
-        for j in new_docs:
-            self.lista2.append({"Clave":j.get('Clave'),"Sensor":j.get('Sensor'),"Value":j.get('Value'),"Fecha":j.get('Fecha')})
+                with open('temp.json','r') as file:
+                    listatempo=json.load(file)
+                db = self.client[db_name]
+                coll = db[coll_name]
+                coll.insert_many(listatempo)
+                os.remove("temp.json")
+            else:
+             db = self.client[db_name]
+             coll = db[coll_name]
+             coll.insert_many(new_docs)
              
-        self.agregarjson(self.lista2,"temp")    
+            
+      
+        except Exception as e:
+              print("No se puede conectar a ningún servidor")
+              for j in new_docs:
+                  self.lista2.append({"Clave":j.get('Clave'),"Sensor":j.get('Sensor'),"Value":j.get('Value'),"Fecha":j.get('Fecha')})
+             
+              self.crearjson(self.lista2,"temp")
+              
+        # If the update fails, write new_docs to "temp.json"
+         
+            
